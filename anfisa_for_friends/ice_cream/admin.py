07eb17142484c9bcc09109910751cffc07f93ab7
Sergey Beschstniy <Diman.9533@gmail.com>
2024-05-14 13:39:32 +0300

    update admin

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category
+from .models import Category, Wrapper, IceCream, Topping
 
 admin.site.register(Category)
+admin.site.register(Wrapper)
+admin.site.register(IceCream)
+admin.site.register(Topping)
